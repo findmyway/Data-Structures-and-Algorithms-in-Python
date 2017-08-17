22e939b80c9806085427b2a951be970ace942003
Madhan <maddymadhan946@gmail.com>
2017-08-17 18:10:02 +0530

    Update C1_13.py

--- ch1/C1_13.py
+++ ch1/C1_13.py
@@ -3,7 +3,7 @@
 # Mail: find_my_way@foxmail.com
 # Created Time: 2013/12/16 11:00:31
 def myRevers(data):
-    return [data[-(i+1)] for i in range(data(l))]
+    return [data[-(i+1)] for i in range(len(data))]
     
 if __name__ == '__main__':
     print(myRevers([1, 2, 3, 4, 5]))
